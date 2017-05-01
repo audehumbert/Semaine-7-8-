3034da0d41c6b2fa2099106828e934402ba82186
Aude <audehumbert@yahoo.fr>
2017-05-01 21:56:32 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Semaine-7-8-
+
+
+
+
+Semaine 7 & 8 en même temps avec un travail mettant en pratique les exercices demandés, mais sur un thème différent du CV, pour changer un peu. 
+
+La version n'est pas finalisée, il reste du travail à fournir...! 
---
+++ js/scrollmovement.js
@@ -0,0 +1,15 @@
+//mouvement fluide
+$(function () {
+    //au click sur chaque lien nous ferons apelle à la fonction animate de Jquery
+    $('nav a').on('click', function (e) {
+        e.preventDefault();
+        // hash permet de cibler le href de nos liens.
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000, function () {
+            window.location.hash = hash;
+        });
+    });
+});
+
